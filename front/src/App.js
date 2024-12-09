7aca00b971c134e9a7a3e6f13ae63a75c841ea41
JJW <mmd011375@gmail.com>
2024-12-09 11:34:11 +0900

    바르미 프로젝트 시작

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './compnents/navi/Navigation';
+
+function App() {
+  return (
+    <div className="mobile-container">
+        <Navigation></Navigation>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/compnents/navi/Navigation.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from 'react'
+import './Navi.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping, faClockRotateLeft, faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import {} from '@fortawesome/free-brands-svg-icons';
+const Navigation = () => {
+
+    const [select, setSelect] = useState('home')
+    
+    const naviRef = useRef();
+
+    const changeNavi = (e) => {
+        const targetName = e.currentTarget.querySelector('p').innerText;
+        setSelect(targetName.toLowerCase());
+    }
+
+  return (
+    <div className='navi-container'>
+        <div className={`main ${select === 'home' ? 'active' : ''}`} onClick={(e) => {changeNavi(e)}} ref={ naviRef }>
+            <FontAwesomeIcon icon={faHouse} style={{
+                color: select === 'home' ? '#001D6E' : ''
+            }}/>
+            <p style={{
+               color: select === 'home' ? '#001D6E' : ''  
+            }}>Home</p>
+        </div>
+        <div className='history' onClick={(e) => {changeNavi(e)}} ref={ naviRef }>
+            <FontAwesomeIcon icon={faClockRotateLeft} style={{
+                color: select === 'history' ? '#001D6E' : ''
+            }}/>
+            <p style={{
+                color: select === 'history' ? '#001D6E' : ''
+            }}>History</p>
+        </div>
+        <div className='product' onClick={(e) => {changeNavi(e)}} ref={ naviRef }>
+            <FontAwesomeIcon icon={faBasketShopping} style={{
+                color: select === 'product' ? '#001D6E' : ''
+            }}/>
+            <p style={{
+                color: select === 'product' ? '#001D6E' : ''
+            }}>Product</p>
+        </div>
+        <div className='my-page' onClick={(e) => {changeNavi(e)}} ref={ naviRef }>
+            <FontAwesomeIcon icon={faUser} style={{
+                color: select === 'mypage' ? '#001D6E' : ''
+            }}/>
+            <p style={{
+                color: select === 'mypage' ? '#001D6E' : ''
+            }}>MyPage</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
