eb5415ca7314b189e4d1c96e06c4e3b4a70347f3
SGC <sgc0216@gmail.com>
2024-12-11 10:50:03 +0900

    마이페이지 작업중 20% - 송건창

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navigation from './compnents/navi/Navigation';
 import Main from './compnents/main/Main';
+import MyPage from './compnents/mypage/MyPage';
 
 function App() {
   // 테스트중
+
   return (
     <div className="mobile-container">
         <Routes>
           <Route path="/" element={<Main/>}></Route>
-          <Route path="/my-page"></Route>
+          <Route path="/my-page" element={<MyPage/>}></Route>
           <Route path="/history"></Route>
           <Route path="/product"></Route>
         </Routes>
